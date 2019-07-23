73bef1c544963f7bfc671d338ea37150c0c6ee57
Omar <o.mohammed@morniksa.com>
2019-07-23 23:36:25 +0200

    init commit

---
+++ src/components/AboutPage.js
@@ -0,0 +1,26 @@
+// import React from "react";
+
+// class AboutPage extends React.component {
+//   render() {
+//     return (
+//       <>
+//         <h2>About</h2>
+//         <p>This app uses React.</p>
+//       </>
+//     );
+//   }
+// }
+
+// export default AboutPage;
+import React from "react";
+
+function AboutPage() {
+  return (
+    <div className="jumbotron">
+      <h1>Hello</h1>
+      <p>This app uses React.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>What ever</h1>
+      <p>omar osama</p>
+    </div>
+  );
+}
+
+export default HomePage;
